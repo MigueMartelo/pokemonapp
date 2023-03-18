64a446471b67cb616f42180bf7e7af1c0b5d8fb5
Migue Martelo <miguemartelo1031@gmail.com>
2023-03-18 13:39:29 -0500

    add PokemonApp component and the pokemonSlice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,14 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+
+      <ul>
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+      </ul>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import './index.css';
+import { PokemonApp } from './PokemonApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 );
